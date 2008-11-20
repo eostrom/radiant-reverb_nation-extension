a2c7b189f530e11a4e2e1532af1f464b615d421a
Erik Ostrom <erik@echographia.com>
2008-11-19 22:09:22 -0800

    Refactored tags to use the new model classes.

--- app/models/reverb_nation_tags.rb
+++ app/models/reverb_nation_tags.rb
@@ -5,40 +5,29 @@ require 'nokogiri'
 module ReverbNationTags
   include Radiant::Taggable
   
-  SHOWS_FEED = "http://reverbnation.com/controller/rss/artist_shows_rss/"
-  
-  def shows_feed(artist_id)
-    open(SHOWS_FEED + artist_id) {|f| f.read}
-  end
-  
   tag 'reverbnation' do |tag|
     tag.locals.artist_id = tag.attr['id']
     if tag.locals.artist_id.blank?
       raise ArgumentError, 'No id specified for ReverbNation'
     end
+    tag.locals.artist = ReverbNation::Artist.new(:id => tag.locals.artist_id)
     tag.locals.country = tag.attr['country']
-    tag.locals.feed = shows_feed(tag.locals.artist_id)
-    tag.locals.xml = Nokogiri::XML(tag.locals.feed)
     
     tag.expand
   end
   
-  {
-    'name' => 'artist_name',
-    'genres' => 'genres',
-    'members' => 'band_members',
-    'label' => 'label',
-    'link' => 'link',
-    'branding' => 'description'
-  }.each do |tagname, rssname|
+  ['name', 'members', 'label', 'link', 'branding']. each do |tagname|
     tag "reverbnation:#{tagname}" do |tag|
-      tag.locals.xml.xpath("//channel/#{rssname}").first.inner_html
+      tag.locals.artist.send(tagname)
     end
   end
   
+  tag "reverbnation:genres" do |tag|
+    tag.locals.artist.genres.join(' / ')
+  end
+  
   tag 'reverbnation:location' do |tag|
-    location = tag.locals.xml.xpath("//channel/location").first.inner_html
-    location.sub(/, #{tag.locals.country}$/, '')
+    tag.locals.artist.location.sub(/, #{tag.locals.country}$/, '')
   end
   
   tag 'reverbnation:shows' do |tag|
@@ -51,47 +40,30 @@ module ReverbNationTags
   end
   
   tag 'reverbnation:shows:each' do |tag|
-    tag.locals.xml.xpath('//item').map do |item|
-      tag.locals.item = item
-      tag.locals.date = Time.parse(item.xpath('title').first.inner_text)
+    tag.locals.artist.shows.map do |show|
+      tag.locals.show = show
       tag.expand
     end
   end
   
-  {
-    'link' => 'link', # currently useless - keep hope alive!
-
-    'ticket_price' => 'ticket_price',
-    'ticket_link' => 'tickets_url',
-
-    'venue' => 'venue',
-    'latitude' => 'latitude',
-    'longitude' => 'longitude',
-    'georss:point' => 'georss:point',
-
-    'note' => 'note'
-  }.each do |tagname, rssname|
+  ['link', 'ticket_price', 'ticket_link',
+    'venue', 'latitude', 'longitude', 'note'
+  ].each do |tagname|
     tag "reverbnation:shows:each:#{tagname}" do |tag|
-      tag.locals.item.xpath(rssname).first.inner_html
+      tag.locals.show.send(tagname)
     end
   end
   
-  def omit_country(tag, element)
-    value = tag.locals.item.xpath(element).first.inner_html
-    country = tag.locals.country
-    if country
-      value.sub(/,? #{country}$/, '').sub(/, *$/, '')
-    else
-      value
+  ['location', 'address'].each do |tagname|
+    tag "reverbnation:shows:each:#{tagname}" do |tag|
+      value = tag.locals.show.send(tagname)
+      country = tag.locals.country
+      if country
+        value.sub(/,? #{country}$/, '').sub(/, *$/, '')
+      else
+        value
+      end
     end
-  end
-  
-  tag "reverbnation:shows:each:location" do |tag|
-    omit_country(tag, 'loc')
-  end
-  
-  tag "reverbnation:shows:each:address" do |tag|
-    omit_country(tag, 'address')
   end
   
   Time::DATE_FORMATS[:show_date] = '%b %e'
@@ -101,12 +73,12 @@ module ReverbNationTags
     tag "reverbnation:shows:each:#{tagname}" do |tag|
       custom = tag.attr['custom']
       if custom
-        tag.locals.date.strftime(custom)
+        tag.locals.show.datetime.strftime(custom)
       else
-        tag.locals.date.to_s("show_#{tag.name}".to_sym)
+        tag.locals.show.datetime.to_s("show_#{tag.name}".to_sym)
       end
     end
   end
   
-  # TODO: artists
+  # TODO: reverbnation:shows:each:artists
 end
